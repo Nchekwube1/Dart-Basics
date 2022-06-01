6e5c948f01fda274f68c81c5bbfb367fe2d64597
Unekwe Francis <unekwenchekwube@gmail.com>
2022-06-01 11:34:26 +0100

    data types

--- first.dart
+++ first.dart
@@ -4,12 +4,30 @@ import 'dart:io';
 
 // every dart program starts with the main function, always make sure to end each expreession with a semi-colon (";")
 void main() {
+// printing
+
   // print('Hello world!!!');
+
+  // Variable assignment
+
   // var firstName = " Francis";
   // String lastName = "Unekwe";
   // print(firstName + " " + lastName);
 
-  stdout.writeln("What is your name?");
-  String userName = stdin.readLineSync() as String;
-  print('Welcome $userName');
+
+  //Stdout and Stdin
+
+
+  // stdout.writeln("What is your name?");
+  // String userName = stdin.readLineSync() as String;
+  // print('Welcome $userName');
+
+
+  // Data types
+
+  // 1. String
+  // 2. Bool
+  // 3. Int
+  // 4. Double
+  // 5. Dynamic
 }
